3e5c7b0ff1085560a83e468c31eee668c7f6a05f
jam7771995 <jam.mason1@gmail.com>
2019-05-21 14:26:37 -0400

    Initial Materials Push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TagOperationsComponent } from './components/tag-operations/tag-operations.component';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MyOwnCustomMaterialModule} from './material'
+
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { TagOperationsComponent } from './components/tag-operations/tag-operatio
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MyOwnCustomMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
