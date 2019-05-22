a50a9bd9c86277b6a4274b129bc340fe3eeab3d4
Daniel Grindstaff <dan@oakrun.net>
2019-05-22 10:29:28 -0400

    "Wednesday morning push new new NavBar component added"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,17 @@ import { AppComponent } from './app.component';
 import { TagOperationsComponent } from './components/tag-operations/tag-operations.component';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MyOwnCustomMaterialModule} from './material';
-import {MatFormFieldModule, MatInputModule} from '@angular/material';
+import {MatFormFieldModule, MatInputModule, MatMenuModule} from '@angular/material';
 import { CreateOperationsComponent } from './components/create-operations/create-operations.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TagOperationsComponent,
-    CreateOperationsComponent
+    CreateOperationsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +24,8 @@ import { CreateOperationsComponent } from './components/create-operations/create
     BrowserAnimationsModule,
     MyOwnCustomMaterialModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
