a1f0749430ccd7c189737afa81a92248ef8b4775
mccollum3d <mccollum3d@gmail.com>
2019-05-22 18:57:05 -0400

    Naming convention / code consistency: updated models to declare variables inside the constructors to match the Tag model, so that all 3 model objects use the same syntax.

--- src/app/model/module.ts
+++ src/app/model/module.ts
@@ -2,10 +2,12 @@
 //Module template, most module functionality will be implemented in future sprints.
 
 export class Module {
-    public id: number;            //Primary key of module
-    public name: string;          //Name of the module
-    public isActive: boolean;      //Is this module active? (i.e. deactivate module for old Java version)
-
+    constructor(
+    public id: number,            //Primary key of module
+    public name: string,          //Name of the module
+    public isActive: boolean      //Is this module active? (i.e. deactivate module for old Java version)
+    ) {
+    }
     //public createdDate: number        //placeholder: time of creation in millis
     //public updatedDate: number        //placeholder: time of update in millis
 }
