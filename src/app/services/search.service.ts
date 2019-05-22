96a8de2412adbd696879d61762a26ec99bb5110c
keisuke_miyazaki <mint@momoirosoft.moe>
2019-05-22 16:33:44 -0400

    Updated search observable to include all, by tag, by tag and category.

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -2,15 +2,23 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Result } from '../model/result.model';
+import { API_URL } from '../app.constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
-  url:string = "urlGoesHereGibby";
   constructor(private http:HttpClient) { }
 
   getAllResults():Observable<Result[]>{
-    return this.http.get<Result[]>(this.url);
+    return this.http.get<Result[]>(API_URL);
+  }
+
+  getResultsByTag(tagId:string):Observable<Result[]>{
+    return this.http.get<Result[]>(API_URL + "/findByTags/tagId");
+  }
+
+  getResultsByTagAndCategory(tagsAndCategory:string):Observable<Result[]>{
+    return this.http.get<Result[]>(API_URL + "/findByTagsAndCategory/tagsAndCategory");
   }
 }
