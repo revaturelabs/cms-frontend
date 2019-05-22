f976260d0c919b34af2ff9bc47c8ada96b081536
mccollum3d <mccollum3d@gmail.com>
2019-05-22 18:57:47 -0400

    Naming convention / code consistency: updated models to declare variables inside the constructors to match the Tag model, so that all 3 model objects use the same syntax.

--- src/app/model/content.ts
+++ src/app/model/content.ts
@@ -3,14 +3,15 @@
 
 export class Content {
     constructor(
-    public id: number,            //Primary key of content
-    public category: string,      //The Category user gave content
-    public description: string,   //The description user gave content
-    public name: string,          //The name user gave content
-    public url: string,           //The url user supplied that links to the content
-    public tags: number[],         //Array of every tag user assigned to this content
+        public id: number,            //Primary key of content
+        public category: string,      //The Category user gave content (ie. code example, notes)
+        public description: string,   //The description user gave content
+        public name: string,          //The name user gave content
+        public url: string,           //The url user supplied that links to the content
+        public tags: number[],        //Array of every tag user assigned to this content
+        //public createdDate: number  //placeholder for created date
+        //public updatedDate: number  //placeholder for updated date
     ) {
     }
-    //public createdDate: number        //placeholder: time of creation in millis
-    //public updatedDate: number        //placeholder: time of update in millis
+
 }
--- src/app/model/module.ts
+++ src/app/model/module.ts
@@ -3,11 +3,11 @@
 
 export class Module {
     constructor(
-    public id: number,            //Primary key of module
-    public name: string,          //Name of the module
-    public isActive: boolean      //Is this module active? (i.e. deactivate module for old Java version)
+        public id: number,            //Primary key of module
+        public name: string,          //Name of the module
+        public isActive: boolean      //Is this module active? (i.e. deactivate module for old Java version)
+        //public createdDate: number  //placeholder for created date
+        //public updatedDate: number  //placeholder for updated date
     ) {
     }
-    //public createdDate: number        //placeholder: time of creation in millis
-    //public updatedDate: number        //placeholder: time of update in millis
 }
