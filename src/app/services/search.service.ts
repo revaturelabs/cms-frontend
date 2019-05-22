dafa0dde0dd23d34223e91c073e08022dac3e340
keisuke_miyazaki <mint@momoirosoft.moe>
2019-05-22 15:47:36 -0400

    search

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Result } from '../model/result.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
+  url:string = "urlGoesHereGibby";
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  getAllResults():Observable<Result[]>{
+    return this.http.get<Result[]>(this.url);
+  }
 }
