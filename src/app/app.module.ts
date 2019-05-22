22076293f659403732fead7377c8702b04ac6f4e
keisuke_miyazaki <mint@momoirosoft.moe>
2019-05-22 16:15:56 -0400

    fixed my app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,33 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TagOperationsComponent } from './components/tag-operations/tag-operations.component';
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
 import {MyOwnCustomMaterialModule} from './material';
 import {MatFormFieldModule, MatInputModule} from '@angular/material';
+import {ResultComponent} from './components/result/result.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TagOperationsComponent
+    TagOperationsComponent,
+    ResultComponent,
+    SearchbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
     BrowserAnimationsModule,
     MyOwnCustomMaterialModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
