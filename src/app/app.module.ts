5255b9bb8facc55978f9fa9101cbefcb7424a8b6
miyazaki1 <kmiyazaki56@gmail.com>
2019-05-23 12:05:42 -0400

    Nav bar endpoints2 (#8)
    * Search result format with dummy data, result component, model, service and environment change
    
    * created sidebar component
    
    * structure
    
    * Updated result model to contain data being sent from back end.
    
    * created searchbar module
    
    * deleted sidebar component
    
    * Change the dummy data to the real function. And observable still wrong
    
    * Began creating proper result component for actual recieved data instead of dummy data.
    
    * Restore
    
    * created search service
    
    * Change for checkout
    
    * created search service
    
    * Seach box is working to add tags to array
    
    * Change the package json
    
    * resolve merge conflicts for materials
    
    * created missing package.json file
    
    * fixed merge conflic in package.json file
    
    * Resolved all merge conflicts with pulled javaee branch.
    
    * added ResultComponent to app-routing.module
    
    * Added longer Select
    
    * moved result components into the components folder and the result services into the services folder. Deleted empty service folder. Fixed all import paths to the new paths for result components. Fixed result model to match dummy data being passed. Fixed any incorrect naming in the result.component.html
    
    * no message
    
    * changed from Request to Results
    
    * search
    
    * fixed my app module
    
    * Updated search observable to include all, by tag, by tag and category.
    
    * fixed the url body and included more searches.
    
    * converted get methods to post on search service.
    
    * Updated the tag comonent
    
    * Checking
    
    * fixed app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,30 +4,39 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TagOperationsComponent } from './components/tag-operations/tag-operations.component';
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
 import {MyOwnCustomMaterialModule} from './material';
 import {MatFormFieldModule, MatInputModule, MatMenuModule} from '@angular/material';
 import {NavbarComponent} from './components/navbar/navbar.component';
 import {HttpClientModule} from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
+import {ResultComponent} from './components/result/result.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TagOperationsComponent,
-    NavbarComponent
+    NavbarComponent,
+    ResultComponent,
+    SearchbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
     BrowserAnimationsModule,
     MyOwnCustomMaterialModule,
     MatFormFieldModule,
     MatInputModule,
     MatMenuModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/result.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import {Result} from '../model/result.model';
+import {Observable} from 'rxjs';
+import 'rxjs';
+//import {Response} from '@angular/http';
+import 'rxjs/Observable';
+@Injectable({
+  providedIn: 'root'
+})
+export class ResultService {
+
+  constructor() { }
+}
