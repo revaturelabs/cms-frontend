c57fc8cbd8195ea16655befd6b91afe1566d8691
fachiolc <fachiolc@gmail.com>
2019-05-23 14:21:51 -0400

    Added search page, moved create operations from navbar to tag-operations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import {TagOperationsComponent} from './components/tag-operations/tag-operations.component';
 import {ResultComponent} from './components/result/result.component';
 import { NavbarComponent} from './components/navbar/navbar.component';
+import { SearchPageComponent} from './components/search-page/search-page.component';
 const routes: Routes = [
   { path: 'tags', component: TagOperationsComponent },
   { path: 'result', component: ResultComponent},
-  { path: 'navbar', component: NavbarComponent }
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'search', component: SearchPageComponent}
 ];
 
 @NgModule({
