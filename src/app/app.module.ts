800122d901d84d6902b8369d7ca107b26aa18414
Dandy Pham <phuccpham96@yahoo.com>
2019-05-24 11:10:39 -0400

    Update material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TagOperationsComponent } from './components/tag-operations/tag-operations.component';
 import { SearchbarComponent } from './components/searchbar/searchbar.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
 import {MyOwnCustomMaterialModule} from './material';
-import {MatFormFieldModule, MatInputModule, MatMenuModule} from '@angular/material';
-import {NavbarComponent} from './components/navbar/navbar.component';
-import {HttpClientModule} from '@angular/common/http';
-import {ResultComponent} from './components/result/result.component';
+import {MatFormFieldModule, MatInputModule,  MatToolbarModule, MatMenuModule, MatIconModule, MatStepperModule} from '@angular/material';
+import {MatListModule} from '@angular/material/list'; 
+import {MatTreeModule} from '@angular/material/tree';
+import {MatCardModule} from '@angular/material/card';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ResultComponent } from './components/result/result.component';
+import { ResultService } from './services/result.service';
 import { SearchPageComponent } from './components/search-page/search-page.component';
 
 
@@ -22,10 +25,11 @@ import { SearchPageComponent } from './components/search-page/search-page.compon
     TagOperationsComponent,
     NavbarComponent,
     ResultComponent,
-    SearchbarComponent,
-    SearchPageComponent
+    SearchPageComponent,
+    SearchbarComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
@@ -35,12 +39,15 @@ import { SearchPageComponent } from './components/search-page/search-page.compon
     MyOwnCustomMaterialModule,
     MatFormFieldModule,
     MatInputModule,
-    MatMenuModule,
-    HttpClientModule,
-    FormsModule,
-    MatButtonModule
+    MatListModule,
+    MatToolbarModule, 
+    MatTreeModule,
+    MatCardModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatMenuModule
   ],
-  providers: [],
+  providers: [ResultService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
