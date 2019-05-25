32cda76c8e49789dc971a00a41cccd69b9d69a30
Gibby Hart <gibbidy_5@hotmail.com>
2019-05-25 16:09:26 -0400

    updated production environment url to ec2 url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true
 };
 
-//export const API_URL = 'http://localhost:8080/cms'; //old url
-export const API_URL = 'http://localhost'; //url for when jenkins build issue gets resolved
+//export const API_URL = 'http://ec2-54-146-4-31.compute-1.amazonaws.com:8080/CmsForce'; //old url
+export const API_URL = 'http://ec2-54-146-4-31.compute-1.amazonaws.com:8080/CmsForce'; //url for when jenkins build issue gets resolved
