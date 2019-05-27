e49575f31833f60792dc392708be7722afa67f50
Gibby Hart <gibbidy_5@hotmail.com>
2019-05-27 09:22:41 -0400

    corrected models to match backend

--- src/app/model/content.ts
+++ src/app/model/content.ts
@@ -1,13 +1,17 @@
+import { Tag } from './tag';
+
 export class Content {
   constructor(
-    public id: number,            // Primary key of content
-    public category: string,      // The Category user gave content (ie. code example, notes)
+    public contentId: number,            // Primary key of content
+    public tags: Tag[],        // Array of every tag user assigned to this content
     public description: string,   // The description user gave content
+    public category: string,      // The Category user gave content (ie. code example, notes)
+    
     public name: string,          // The tagName user gave content
     public url: string,           // The url user supplied that links to the content
-    public tags: number[],        // Array of every tag user assigned to this content
-    public createdDate: Date,  // placeholder for created date
-    public updatedDate: Date  // placeholder for updated date
+    
+    public dateCreated: Date,  // placeholder for created date
+    public dateUpdated: Date  // placeholder for updated date
   ) {
   }
 }
