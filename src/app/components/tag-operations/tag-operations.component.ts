67e6cbcf9399adb10bb1ccd5b009185a8c1bdabd
Daniel Grindstaff <dan@oakrun.net>
2019-05-28 15:00:34 -0400

    Better regex string added

--- src/app/components/tag-operations/tag-operations.component.ts
+++ src/app/components/tag-operations/tag-operations.component.ts
@@ -23,8 +23,8 @@ export class TagOperationsComponent implements OnInit {
   modules: string[];
   inputContent = new InputContentDTO(new Content(0, [], '', '', '', '', null, null),  [], new Module(0, '', true, null, null));
 
-  public myreg = /(^|\s)((https?:\/\/)?[\w-]+(\.[\w-]+)+\.?(:\d+)?(\/\S*)?)/gi;
-
+  // public myreg = /(^|\s)((https?:\/\/)?[\w-]+(\.[\w-]+)+\.?(:\d+)?(\/\S*)?)/gi;
+  public myreg = /^(http?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/;
   url = new FormControl('', [Validators.required, Validators.pattern(this.myreg)]);
 
   constructor(
